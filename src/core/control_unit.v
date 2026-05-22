e4f9c7e7aad6d15f2c8e108d5b54ff266af11f05
SairamKesanapalli <sairamkesanapalli@gmail.com>
2026-05-22 15:05:05 +0530

    Updates control unit

--- src/core/control_unit.v
+++ src/core/control_unit.v
@@ -47,7 +47,7 @@ module control_unit (
         //initializing all the default values to 0 to prevent latch interference
         ALUSrcA_ctrl = 2'b00; // A
         ALUSrcB_ctrl = 2'b00; // B
-        ALU_OP       = 3'd0;  // R-Type or M-Type
+        ALU_OP       = 3'd0;  
         MemRead      = 0;
         MemWrite     = 0;
         Branch       = 0;
@@ -61,84 +61,84 @@ module control_unit (
                 ALUSrcA_ctrl = 2'b01; // A
                 ALUSrcB_ctrl = 2'b00; // B
                 ALU_OP       = 3'd2;  // R-Type or M-Type
-                MemRead  = 0;
-                MemWrite = 0;
-                Branch   = 0;
-                Jump     = 0;
-                RegWrite = 1;
-                MemToReg = 0;
+                MemRead      = 0;
+                MemWrite     = 0;
+                Branch       = 0;
+                Jump         = 0;
+                RegWrite     = 1;
+                MemToReg     = 0;
             end
             7'b0010011:begin                                     // I-type
                 // ALUOut = A op imm
                 ALUSrcA_ctrl = 2'b01; // A
                 ALUSrcB_ctrl = 2'b10; // imm
                 ALU_OP       = 3'd0;  // I-Type
-                MemRead  = 0;
-                MemWrite = 0;
-                Branch   = 0;
-                Jump     = 0;
-                RegWrite = 1;
-                MemToReg = 0;
+                MemRead      = 0;
+                MemWrite     = 0;
+                Branch       = 0;
+                Jump         = 0;
+                RegWrite     = 1;
+                MemToReg     = 0;
             end
             7'b0000011:begin                 
                 // ALUOut = A + imm (Address calculation)        // LW
                 ALUSrcA_ctrl = 2'b01; // A
                 ALUSrcB_ctrl = 2'b10; // imm
                 ALU_OP       = 3'd3;  // ADD                    
-                MemRead  = 1;
-                MemWrite = 0;   
-                Branch   = 0;
-                Jump     = 0;
-                RegWrite = 1;
-                MemToReg = 1;
+                MemRead      = 1;
+                MemWrite     = 0;   
+                Branch       = 0;
+                Jump         = 0;
+                RegWrite     = 1;
+                MemToReg     = 1;
             end
             7'b0100011:begin                                     // SW
                 // ALUOut = A + imm (Address calculation)
                 ALUSrcA_ctrl = 2'b01; // A
                 ALUSrcB_ctrl = 2'b10; // imm
                 ALU_OP       = 3'd3;  // ADD
-                MemRead  = 0;
-                MemWrite = 1;
-                Branch   = 0;
-                Jump     = 0;
-                RegWrite = 0;
-                MemToReg = 0;
+                MemRead      = 0;
+                MemWrite     = 1;
+                Branch       = 0;
+                Jump         = 0;
+                RegWrite     = 0;
+                MemToReg     = 0;
             end
             7'b1100011:begin                                    // B-type (BEQ) 
                 // Compute A - B to set flags
                 ALUSrcA_ctrl = 2'b01; // A
                 ALUSrcB_ctrl = 2'b00; // B
                 ALU_OP       = 3'd1;  // SUB for Branch
-                MemRead  = 0;
-                MemWrite = 0;   
-                Branch   = 1;    // Control signal to indicate this is a branch instruction
-                Jump     = 0;
-                RegWrite = 0;
-                MemToReg = 0;
+                MemRead      = 0;
+                MemWrite     = 0;   
+                Branch       = 1;    // Control signal to indicate this is a branch instruction
+                Jump         = 0;
+                RegWrite     = 0;
+                MemToReg     = 0;
             end
             7'b1101111:begin                                       // JAL
                 // PC = PC + imm (using ALU), Reg = PC+4 (from ALUOut)
                 ALUSrcA_ctrl = 2'b00; // PC
                 ALUSrcB_ctrl = 2'b01; // 4
                 ALU_OP       = 3'd3;  // ADD 
-                MemRead  = 0;
-                MemWrite = 0;
-                Branch   = 0;
-                Jump     = 1;
-                RegWrite = 1;
-                MemToReg = 0;     
+                MemRead      = 0;
+                MemWrite     = 0;
+                Branch       = 0;
+                Jump         = 1;
+                RegWrite     = 1;
+                MemToReg     = 0;     
             end
             7'b1100111:begin                                        // JALR
                 // PC = (A + imm) & ~1, Reg = PC+4 (from ALUOut)
                 ALUSrcA_ctrl = 2'b00; // PC
                 ALUSrcB_ctrl = 2'b01; // 4
                 ALU_OP       = 3'd3;  // ADD
-                MemRead  = 0;
-                MemWrite = 0;
-                Branch   = 0;
-                Jump     = 1;
-                RegWrite = 1;
-                MemToReg = 0;
+                MemRead      = 0;
+                MemWrite     = 0;
+                Branch       = 0;
+                Jump         = 1;
+                RegWrite     = 1;
+                MemToReg     = 0;
             end   
 
             7'b0110111: begin           // U-TYPE => LUI
@@ -146,28 +146,38 @@ module control_unit (
                 ALUSrcA_ctrl = 2'b10; // 0
                 ALUSrcB_ctrl = 2'b10; // imm
                 ALU_OP       = 3'd3;  // ADD
-                RegWrite = 1;
-                MemRead  = 0;
-                MemWrite = 0;
-                MemToReg = 0;
-                Branch   = 0;
-                Jump     = 0;
-                ALU_OP   = 3'd3;
+                RegWrite     = 1;
+                MemRead      = 0;
+                MemWrite     = 0;
+                MemToReg     = 0;
+                Branch       = 0;
+                Jump         = 0;
+                ALU_OP       = 3'd3;
             end
             7'b0010111: begin           // U-TYPE => AUIPC
                 // AUIPC, Branches, Memory: Compute target early! ALUOut = PC + imm
                 ALUSrcA_ctrl = 2'b00; // PC
                 ALUSrcB_ctrl = 2'b10; // imm
                 ALU_OP       = 3'd3;  // ADD
-                RegWrite = 1;
-                MemRead  = 0;
-                MemWrite = 0;
-                MemToReg = 0;
-                Branch   = 0;
-                Jump     = 0;
-                ALU_OP   = 3'd3;
+                RegWrite     = 1;
+                MemRead      = 0;
+                MemWrite     = 0;
+                MemToReg     = 0;
+                Branch       = 0;
+                Jump         = 0;
+                ALU_OP       = 3'd3;
             end
-
+            default: begin
+                ALUSrcA_ctrl = 2'b00; // A
+                ALUSrcB_ctrl = 2'b00; // B
+                ALU_OP       = 3'd0;  
+                MemRead      = 0;
+                MemWrite     = 0;
+                Branch       = 0;
+                Jump         = 0;
+                RegWrite     = 0;
+                MemToReg     = 0;
+            end
         endcase
     end
 endmodule
